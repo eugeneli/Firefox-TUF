02d967310ed78e93f3ea2147b53f3df25b66f82d
Eugene Li <hello@itseugene.li>
2013-11-12 17:47:03 -0500

    Put missing interposition url

--- TUF/proxyserver.py
+++ TUF/proxyserver.py
@@ -4,7 +4,7 @@ from tuf.interposition import urllib2_tuf as urllib2
 
         
 urls = (
-    '/(.*)', 'tufguy'
+   "http://aus3.mozilla.org/update/3/Firefox/24.0/20130910160258/Linux_x86_64-gcc3/en-US/release/Linux%202.6.32-358.23.2.el6.x86_64%20(GTK%202.18.9)/default/default/update.xml?force=1", "tufguy"
 )
 app = web.application(urls, globals())
 
