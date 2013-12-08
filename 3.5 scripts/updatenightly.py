448c3d4aa0b3d9b5d5dde528b92f6495586c08fe
eugeneli <hello@itseugene.li>
2013-12-08 02:37:35 -0500

    Scripts now take path to password files

--- 3.5 scripts/updatenightly.py
+++ 3.5 scripts/updatenightly.py
@@ -1,32 +1,44 @@
 from tuf.libtuf import *
 import distutils.core
+import sys
 
 repository = load_repository("repository/")
-#added following 6 lines of code DM
-private_targets_key = import_rsa_privatekey_from_file("keystore/targets/targets_key", password=targetsPassword)
-private_release_key = import_rsa_privatekey_from_file("keystore/release/release_key", password=releasePassword)
-private_timestamp_key = import_rsa_privatekey_from_file("keystore/timestamp/timestamp_key", password=timestampPassword)
 
-repository.targets.load_signing_key(private_targets_key)
-repository.release.load_signing_key(private_release_key)
-repository.timestamp.load_signing_key(private_timestamp_key)
+if len(sys.argv) == 4:
+	releaseKeyFile = sys.argv[1]
+	f = open(targetsKeyFile)
+	releasePassword = f.readline()
 
-private_nightly_key = import_rsa_privatekey_from_file("keystore/nightly/nightly_key", password="asd123")
-repository.targets.nightly.load_signing_key(private_nightly_key)
+	timestampKeyFile = sys.argv[2]
+	f = open(targetsKeyFile)
+	timestampPassword = f.readline()
 
-#clear current targets
-for target in repository.targets.nightly.target_files:
-	repository.targets.nightly.remove_target("repository/targets/"+target)
+	nightlyKeyFile = sys.argv[3]
+	f = open(targetsKeyFile)
+	nightlyPassword = f.readline()
 
-#add new targets
-nightly_targets = repository.get_filepaths_in_directory("repository/targets/pub/mozilla.org/firefox/nightly/",recursive_walk=True, followlinks=True) 
-repository.targets.nightly.add_targets(nightly_targets)
-#repository.targets.nightly.version = repository.targets.nightly.version + 1
+	private_release_key = import_rsa_privatekey_from_file("keystore/release/release_key", password=releasePassword)
+	private_timestamp_key = import_rsa_privatekey_from_file("keystore/timestamp/timestamp_key", password=timestampPassword)
+	private_nightly_key = import_rsa_privatekey_from_file("keystore/nightly/nightly_key", password=nightlyPassword)
 
-# repository.write_partial() replaced with following - DM
-repository.write
+	repository.release.load_signing_key(private_release_key)
+	repository.timestamp.load_signing_key(private_timestamp_key)
+	repository.targets.nightly.load_signing_key(private_nightly_key)
 
-# copy subdirectory example
-stagedMetadata = "repository/metadata.staged"
-metadata = "repository/metadata"
-distutils.dir_util.copy_tree(stagedMetadata, metadata)
+	#clear current targets
+	for target in repository.targets.nightly.target_files:
+		repository.targets.nightly.remove_target("repository/targets/"+target)
+
+	#add new targets
+	nightly_targets = repository.get_filepaths_in_directory("repository/targets/pub/mozilla.org/firefox/nightly/",recursive_walk=True, followlinks=True) 
+	repository.targets.nightly.add_targets(nightly_targets)
+	#repository.targets.nightly.version = repository.targets.nightly.version + 1
+
+	repository.write
+
+	# copy subdirectory example
+	stagedMetadata = "repository/metadata.staged"
+	metadata = "repository/metadata"
+	distutils.dir_util.copy_tree(stagedMetadata, metadata)
+else:
+	print "updatenightly.py [release password path] [timmpstamp password path] [nightly password path]"
